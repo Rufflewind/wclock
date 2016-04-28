a7f641bcd2b988858bc452ea772d55039bb32406
Phil Ruffwind <rf@rufflewind.com>
2016-04-27 21:43:00 -0400

    Forgot to actually use WCLOCK_EXTERN

--- src/wclock.h
+++ src/wclock.h
@@ -23,6 +23,7 @@ typedef struct { double _data; } wclock;
     @return
     Zero on success, nonzero on failure.
 */
+WCLOCK_EXTERN
 int init_wclock(wclock *clock);
 
 /** Retrieve the time from a monotonic wall clock in seconds.
@@ -38,6 +39,7 @@ int init_wclock(wclock *clock);
     is at worst (for an ideal system with hundreds of years in uptime) limited
     to about a few microseconds.  In practice, it is usually much less.
 */
+WCLOCK_EXTERN
 double get_wclock(const wclock *wclock);
 
 /** Obtain the resolution of the monotonic wall clock in seconds.
@@ -49,6 +51,7 @@ double get_wclock(const wclock *wclock);
     Duration relative to some unspecified reference time in seconds.
     If an error occurs, `NAN` is returned.
 */
+WCLOCK_EXTERN
 double get_wclock_res(const wclock *wclock);
 
 #ifdef __cplusplus
